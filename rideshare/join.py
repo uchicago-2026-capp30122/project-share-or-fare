a3ad41aa90865d9506a1c52659c04bd70c6eb984
Molly Long <mrlong@uchicago.edu>
2026-03-02 19:25:15 -0600

    updated data processes to include month variable in small_medium_merged dataset

--- rideshare/join.py
+++ rideshare/join.py
@@ -39,15 +39,14 @@ def join_api_csv(rideshare_data_name, large = False) -> pd.DataFrame:
     transit_data = pd.concat(transit_datasets, axis=0)
     rideshare_data = pd.read_csv(f"./data/{rideshare_data_name}.csv")
 
-    # Set the index column before running pd.join (faster than pd.merge)
-    transit_data.set_index("group_id", inplace=True)
-    rideshare_data.set_index("group_id", inplace=True)
-
     # Perform a join to get all rideshare data that has a matching transit call
     #   Left join with transit on the left, rideshare on the right.
     #   Transit primary key: group_id to rideshare foreign key: group_id
-    #   Effectivley we are getting all rides for which we have a tranist
+    #   Effectivley we are getting all rides for which we have a transit
     #   api call for.
-    rideshare_transit_data = transit_data.join(rideshare_data, how='left')
-
+    rideshare_transit_data = rideshare_data.merge(
+        transit_data,
+        on="group_id",
+        how="inner"
+    )
     return rideshare_transit_data
