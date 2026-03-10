13f286e82a283879b28c185a4b3158b9dd55ba80
Sabrina Wang <sabrina@Sabrinas-Justin-Pro-2019.local>
2026-03-09 22:52:24 -0500

    Test dataset_sample

--- tests/test_visualization_transform_data.py
+++ tests/test_visualization_transform_data.py
@@ -21,20 +21,37 @@ def mini_df():
         }
     )
 
-# @patch("dashboard.visualization.transform.random.randint", return_value=4)
-# def test_dataset_sample_mini(mock_randint, mini_df):
-#     expected_result = pd.DataFrame(
-#         {
-#             "Count": [2],
-#             "rideshareTime": [100],
-#             "totalTransitTime": [11],
-#             "Average Trip Seconds": [134],
-#             "transitPenalty": [2.1],
-#         }
-#     )
 
-#     sample_df = dataset_sample(mini_df, 10)
-#     assert sample_df == expected_result
+@patch("dashboard.visualization.transform.random.randint", return_value=0)
+def test_dataset_sample_tens_includes(mock_randint, mini_df):
+    sample_df = dataset_sample(mini_df, 10)
+    assert len(sample_df) == 1
+    assert sample_df['Average Trip Seconds'].iloc[0] == 120
+
+
+@patch("dashboard.visualization.transform.random.randint", return_value=2)
+def test_dataset_sample_tens_not_includes(mock_randint, mini_df):
+    sample_df = dataset_sample(mini_df, 10)
+    assert len(sample_df) == 0
+
+
+@patch("dashboard.visualization.transform.random.randint", return_value=20)
+def test_dataset_sample_hundreds_includes(mock_randint, mini_df):
+    sample_df = dataset_sample(mini_df, 100)
+    assert len(sample_df) == 1
+    assert sample_df['Average Trip Seconds'].iloc[0] == 120
+
+
+@patch("dashboard.visualization.transform.random.randint", return_value=65)
+def test_dataset_sample_hundreds_not_includes(mock_randint, mini_df):
+    sample_df = dataset_sample(mini_df, 100)
+    assert len(sample_df) == 0
+
+
+@patch("dashboard.visualization.transform.random.randint", return_value=4)
+def test_dataset_sample_remove_long_trips(mock_randint, mini_df):
+    sample_df = dataset_sample(mini_df, 10)
+    assert len(sample_df) == 0
 
 
 def test_weighted_median_mini(mini_df):
